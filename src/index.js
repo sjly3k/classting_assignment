8d0da9aea7e1bcc454cd03e5bfd916e60df9020d
sjly3k <sjly3k@naver.com>
2021-02-05 18:34:48 +0900

    Fix : devTools 호출 오류

--- src/index.js
+++ src/index.js
@@ -16,8 +16,7 @@ import App from './App';
 import rootReducer from './store/modules';
 import GlobalStyle from "./styles/globalStyles";
 
-const devTools =
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk), devTools));
 
